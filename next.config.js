80825273a9ba5a8405fc754232f3752d83af3c07
Jay <joypatel1337@proton.me>
2024-01-06 17:06:16 +0530

    dist pushed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 }
